e1979da4e03339ec747553ff8daeec387207d2d2
Andrew Sprouse <sprouse.a@gmail.com>
2018-11-14 12:02:37 -0500

    Update client to use additional movie info

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,13 @@ const TO_WATCH_FRAGMENT = gql`
   fragment MovieInfo on ToWatch {
     watched
     movieTitle
+    movie {
+      title
+      year
+      director
+      poster
+      rating
+    }
   }
 `;
 
@@ -118,7 +125,23 @@ function RemoveButton({title}) {
   );
 }
 
-function Movie({movieTitle, watched}) {
+function Movie({movie, movieTitle, watched}) {
+  if (movie.title) {
+    return (
+      <div className="Movie">
+        <WatchedCheckbox title={movieTitle} watched={watched}/>
+        <img className="Movie-poster" src={movie.poster} alt={`${movie.title} poster`}/>
+        <div className="Movie-info">
+          <h3>{movie.title} - {movie.year}</h3>
+          <p>{movie.director}</p>
+          <p>{movie.actors}</p>
+          <p>IMDB Rating: {movie.rating}/10</p>
+        </div>
+        <RemoveButton title={movieTitle}/>
+      </div>
+    );
+  }
+
   return (
     <div className="Movie">
       <WatchedCheckbox title={movieTitle} watched={watched}/>
